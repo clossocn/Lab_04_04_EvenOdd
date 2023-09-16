bb7da8a7e56211c0bc8ba4d6f24dd3382be38829
clossocn <clossocn@mail.uc.edu>
2023-09-16 15:20:58 -0400

    Initial commit

---
+++ src/main/java/com/example/lab_04_04_evenodd/Lab_04_04_EvenOdd.java
@@ -0,0 +1,16 @@
+package com.example.lab_04_04_evenodd;
+
+public class Lab_04_04_EvenOdd {
+        public static void main(String[] args) {
+            int inputNumber = 42;
+
+            if (inputNumber % 2 == 0) {
+                System.out.println(inputNumber + " is even.");
+            } else {
+                System.out.println(inputNumber + " is odd.");
+            }
+        }
+
+
+}
+
